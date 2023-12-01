18a469836f3925d0758d9a061ea7a1cb5e213ffc
Maximiliano Rodrigo Soria <MaximilianoRodrigoSoria@gmail.com>
2023-12-01 15:04:48 -0300

    Se agregan logs

--- src/main/java/ar/com/laboratory/openapirestexample/config/OpenApiConfig.java
+++ src/main/java/ar/com/laboratory/openapirestexample/config/OpenApiConfig.java
@@ -18,12 +18,35 @@ import org.springframework.stereotype.Component;
 public class OpenApiConfig
 {
 
-    @Value("${context.path}")
+    @Value("${open.api.context.path}")
     private String contextPath;
 
     @Value("${info.build.version}")
     private String version;
 
+    @Value("${open.api.contact.info.name}")
+    private String contactName;
+
+    @Value("${open.api.contact.info.email}")
+    private String contactEmail;
+
+    @Value("${open.api.contact.info.url}")
+    private String contactUrl;
+
+    @Value("${open.api.contact.info.title}")
+    private String title;
+
+    @Value("${open.api.contact.info.description}")
+    private String description;
+
+    @Value("${open.api.server.url}")
+    private String hostUrl;
+
+    @Value("${open.api.server.description}")
+    private String hostDescription;
+
+    @Value("${open.api.external.doc}")
+    private String externalDoc;
 
     @Bean
     public OpenAPI customOpenAPI(@Value("${info.build.version}") String version) {
@@ -32,16 +55,15 @@ public class OpenApiConfig
                         .addSecuritySchemes("basicScheme", new SecurityScheme().type(SecurityScheme.Type.HTTP)
                                 .scheme("basic")))
                 .info(new Info()
-                        .title("Spaceship Reservation API")
-                        .description("It is and inventory  of spaceships.")
-                        .contact(new Contact().name("Jhon Doe").email("jhondoe@test.com").url("www.test.com"))
+                        .title(title)
+                        .description(description)
+                        .contact(new Contact().name(contactName).email(contactEmail).url(contactUrl))
                         .version(version)
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .addServersItem(new Server().url("http://localhost:8080".concat(contextPath)).description("Local host"))
-                .addServersItem(new Server().url("https://dev.spaceships.com".concat(contextPath)).description("Development environment"))
+                .addServersItem(new Server().url(hostUrl.concat(contextPath)).description(hostDescription))
                 .externalDocs(new ExternalDocumentation()
                         .description("Find more")
-                        .url("https://info.spaceships.com"));
+                        .url(externalDoc));
                  }
     @Bean
     public GroupedOpenApi spaceshipApi() {
